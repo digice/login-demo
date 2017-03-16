e1d9d20221af1eb9347769fc3774d40f224a6b1e
Roderic Linguri <linguri@digices.com>
2017-03-16 16:06:40 -0600

    remove empty indents

--- swift/AppDataManager.swift
+++ swift/AppDataManager.swift
@@ -9,28 +9,28 @@
 import Foundation
 
 class AppDataManager {
-    
+
     static let shared = AppDataManager()
-    
+
     var data : AppData
-    
+
     private init() {
-        
+
         if let storedData = UserDefaults.standard.object(forKey: "appData") as? Data {
             self.data = (NSKeyedUnarchiver.unarchiveObject(with: storedData) as? AppData)!
         } else {
             self.data = AppData()
             self.save()
         }
-    
+
     }
-    
+
     func save() {
-        
+
         let defaults = UserDefaults.standard
         let dataToSave = NSKeyedArchiver.archivedData(withRootObject: self.data)
         defaults.set(dataToSave, forKey: "appData")
-        
+
     }
-    
+
 }
