063f9792a898965f2fd4e699acd95b5c48821811
Roderic Linguri <linguri@digices.com>
2017-03-16 19:59:09 -0600

    remove extra spaces

--- swift/LoginResponse.swift
+++ swift/LoginResponse.swift
@@ -11,36 +11,35 @@ import Foundation
 class LoginResponse {
         
     var timestamp : Int = 0
-    
+
     var authenticated : Bool = false
-    
+
     var token : String = ""
-    
+
     var message : String = ""
-    
+
     func parse(data: Data) {
-        
+
         let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:String]
-        
+
         if let timestamp = json?["timestamp"] {
             self.timestamp = Int(timestamp)!
         }
-        
+
         if let authenticated = json?["authenticated"] {
             if authenticated == "true" {
                 self.authenticated = true
             }
         }
-        
+
         if let token = json?["token"] {
             self.token = token
         }
-        
+
         if let message = json?["message"] {
             self.message = message
         }
-        
+
     }
-    
     
 }
